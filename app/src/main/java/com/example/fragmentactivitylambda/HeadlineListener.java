95876e704e8312023b650a34516f14bcc308c4b4
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-09-27 23:24:32 -0700

    Update HeadlineListener.java

--- app/src/main/java/com/example/fragmentactivitylambda/HeadlineListener.java
+++ app/src/main/java/com/example/fragmentactivitylambda/HeadlineListener.java
@@ -3,6 +3,17 @@ package com.example.fragmentactivitylambda;
 import android.app.Activity;
 import java.io.Serializable;
 
+/**
+ * The listener interface which is independent of the activity. 
+ * It must have a generic type to trigger target type inference.
+ * It is serializable to ensure the listener can survive rotations.
+ */
 public interface HeadlineListener<T extends Activity> extends Serializable {
+  /**
+   * Single method to get a functional interface. It uses the activity type 
+   * {@code T} to trigger type inference. 
+   * @param activity any activity instance.
+   * @para position the second param.
+   */
   void onArticleSelected(T activity, int position);
 }
